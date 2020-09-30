c6b2fdd90907103c878006720ee139f375ef6953
Music <musicm987@gmail.com>
2020-09-29 18:09:46 -0600

    added to class files and added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
 Purchase.destroy_all
 Customer.destroy_all
 Sundae.destroy_all
+
+# topping = [
+#     "sprinkles",
+#     "hot fudge",
+#     "cheesecake bites",
+#     "m&ms",
+#     "peanut butter cups"
+# ]
+
+# ice_cream = [
+#     'vanilla',
+#     'chocolate',
+#     'pistachio',
+#     'caramel',
+#     'rainbow'
+# ]
+
+# sundae1 = Sundae.create(ice_cream: ['vanilla', 'chocolate'], topping: ['sprinkles', 'hot fudge', 'rainbow'])
+
+# user_input = Purchase.create(sundae_id: sundae1.id)
